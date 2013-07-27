90da08e990cf3cbacd34f38f69d564d1825125b6
Artur Malabarba <bruce.connor.am@gmail.com>
2013-07-27 12:22:36 +0100

    Sat 27 Jul 12:22:36 BST 2013

---
+++ ALL/ALL-2013-07-27-new-since-4.el
@@ -0,0 +1,5 @@
+'(
+  (vc-check-status . [(20130726 2125) nil "Warn you when quitting emacs and leaving repo dirty. [github]" tar "melpa"])
+  (pickup . [(0 0 1) nil "pickup file. " single "marmalade"])
+  (fit-frame . [(0) nil "Resize a frame.  In particular, fit a frame to its buffers." single "marmalade"])
+ )
---
+++ marmalade/marmalade-2013-07-27-updated-since-4.el
@@ -0,0 +1,4 @@
+'(
+  (pickup . [(0 0 1) nil "pickup file. " single "marmalade"])
+  (fit-frame . [(0) nil "Resize a frame.  In particular, fit a frame to its buffers." single "marmalade"])
+ )
