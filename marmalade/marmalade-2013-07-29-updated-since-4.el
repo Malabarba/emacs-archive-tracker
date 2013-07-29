2bd2ef941d2bed5b67334d8f0419fe9e03fd2c88
Artur Malabarba <bruce.connor.am@gmail.com>
2013-07-29 16:23:31 +0100

    Mon 29 Jul 16:23:31 BST 2013

--- marmalade/marmalade-2013-07-29-updated-since-4.el
+++ marmalade/marmalade-2013-07-29-updated-since-4.el
@@ -5,4 +5,5 @@
   (psgml . [(1 4 1) nil "Lennart Staflin's Psgml package, with Elisp syntax and sgml-validate fixed for Emacsen >=24." tar "marmalade"])
   (robe . [(0 7 6) ((inf-ruby (2 3 0))) "Code navigation, documentation lookup and completion for Ruby" tar "marmalade"])
   (evil-nerd-commenter . [(0 0 9) nil "Comment/uncomment lines efficiently. Like Nerd Commenter in Vim" tar "marmalade"])
+  (auto-indent-mode . [(0 103) nil "Auto indent Minor mode" single "marmalade"])
  )
