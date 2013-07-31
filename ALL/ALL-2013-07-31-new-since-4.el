73328b7d96bde2e8d7881c6ffcac7651db0d5935
Artur Malabarba <bruce.connor.am@gmail.com>
2013-07-31 13:53:40 +0100

    Wed 31 Jul 13:53:40 BST 2013

--- ALL/ALL-2013-07-31-new-since-4.el
+++ ALL/ALL-2013-07-31-new-since-4.el
@@ -1,6 +1,7 @@
 '(
   (fit-frame . [(20130723 2311) nil "Resize a frame.  In particular, fit a frame to its buffers. [wiki]" single "melpa"])
   (vc-check-status . [(20130730 1656) nil "Warn you when quitting emacs and leaving repo dirty. [github]" tar "melpa"])
+  (py-import-check . [(0 2) nil "Finds the unused python imports using importchecker" single "marmalade"])
   (cubicle-mode . [(0 1) nil "Cubicle major mode for emacs" single "marmalade"])
   (pickup . [(0 0 3) nil "pickup file. " single "marmalade"])
  )
--- marmalade/marmalade-2013-07-31-new-since-4.el
+++ marmalade/marmalade-2013-07-31-new-since-4.el
@@ -1,4 +1,5 @@
 '(
+  (py-import-check . [(0 2) nil "Finds the unused python imports using importchecker" single "marmalade"])
   (cubicle-mode . [(0 1) nil "Cubicle major mode for emacs" single "marmalade"])
   (pickup . [(0 0 3) nil "pickup file. " single "marmalade"])
   (fit-frame . [(0) nil "Resize a frame.  In particular, fit a frame to its buffers." single "marmalade"])
