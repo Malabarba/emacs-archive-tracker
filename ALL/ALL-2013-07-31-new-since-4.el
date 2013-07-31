31eee0f340044139bdd944977e95164ee8a7883d
Artur Malabarba <bruce.connor.am@gmail.com>
2013-07-31 09:54:43 +0100

    Wed 31 Jul 09:54:43 BST 2013

---
+++ ALL/ALL-2013-07-31-new-since-4.el
@@ -0,0 +1,6 @@
+'(
+  (fit-frame . [(20130723 2311) nil "Resize a frame.  In particular, fit a frame to its buffers. [wiki]" single "melpa"])
+  (vc-check-status . [(20130730 1656) nil "Warn you when quitting emacs and leaving repo dirty. [github]" tar "melpa"])
+  (cubicle-mode . [(0 1) nil "Cubicle major mode for emacs" single "marmalade"])
+  (pickup . [(0 0 3) nil "pickup file. " single "marmalade"])
+ )
---
+++ marmalade/marmalade-2013-07-31-updated-since-4.el
@@ -0,0 +1,12 @@
+'(
+  (cubicle-mode . [(0 1) nil "Cubicle major mode for emacs" single "marmalade"])
+  (pickup . [(0 0 3) nil "pickup file. " single "marmalade"])
+  (fit-frame . [(0) nil "Resize a frame.  In particular, fit a frame to its buffers." single "marmalade"])
+  (git-gutter+ . [(0 2) nil "Manage Git hunks straight from the buffer" single "marmalade"])
+  (org-trello . [(0 0 8) ((org (7 9 2)) (dash (1 4 0)) (request (0 1 0)) (cl-lib (0 3 0)) (json (1 2))) "Minor mode for org-mode to sync org-mode and trello" tar "marmalade"])
+  (evil-leader . [(0 3 4) ((evil (0))) "let there be <leader>" single "marmalade"])
+  (psgml . [(1 4 1) nil "Lennart Staflin's Psgml package, with Elisp syntax and sgml-validate fixed for Emacsen >=24." tar "marmalade"])
+  (robe . [(0 7 6) ((inf-ruby (2 3 0))) "Code navigation, documentation lookup and completion for Ruby" tar "marmalade"])
+  (evil-nerd-commenter . [(0 0 9) nil "Comment/uncomment lines efficiently. Like Nerd Commenter in Vim" tar "marmalade"])
+  (auto-indent-mode . [(0 103) nil "Auto indent Minor mode" single "marmalade"])
+ )
