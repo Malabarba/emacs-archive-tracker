a92ca9fdeb43027c61521d46615627ff750a714d
Artur Malabarba <bruce.connor.am@gmail.com>
2013-08-02 13:09:08 +0100

    Fri  2 Aug 13:09:08 BST 2013

---
+++ ALL/ALL-2013-08-02-new-since-4.el
@@ -0,0 +1,12 @@
+'(
+  (fit-frame . [(20130723 2311) nil "Resize a frame.  In particular, fit a frame to its buffers. [wiki]" single "melpa"])
+  (org-pandoc . [(20130729 2050) nil "Export from Org using Pandoc [github]" tar "melpa"])
+  (palimpsest . [(20130731 1821) nil "Various deletion strategies when editing [github]" single "melpa"])
+  (perl-completion . [(20090528 1536) nil "No description available. [github]" single "melpa"])
+  (vc-check-status . [(20130730 1656) nil "Warn you when quitting emacs and leaving repo dirty. [github]" tar "melpa"])
+  (wgrep-ag . [(20130725 1831) ((wgrep (2 1 5))) "Writable ag buffer and apply the changes to files [github]" single "melpa"])
+  (flymake-puppet . [(1 0 0) ((flymake-easy (0 9))) "An Emacs flymake handler for syntax-checking puppet using puppet-lint" single "marmalade"])
+  (py-import-check . [(0 2) nil "Finds the unused python imports using importchecker" single "marmalade"])
+  (cubicle-mode . [(0 1) nil "Cubicle major mode for emacs" single "marmalade"])
+  (pickup . [(0 0 3) nil "pickup file. " single "marmalade"])
+ )
---
+++ melpa/melpa-2013-08-02-new-since-4.el
@@ -0,0 +1,8 @@
+'(
+  (fit-frame . [(20130723 2311) nil "Resize a frame.  In particular, fit a frame to its buffers. [wiki]" single "melpa"])
+  (org-pandoc . [(20130729 2050) nil "Export from Org using Pandoc [github]" tar "melpa"])
+  (palimpsest . [(20130731 1821) nil "Various deletion strategies when editing [github]" single "melpa"])
+  (perl-completion . [(20090528 1536) nil "No description available. [github]" single "melpa"])
+  (vc-check-status . [(20130730 1656) nil "Warn you when quitting emacs and leaving repo dirty. [github]" tar "melpa"])
+  (wgrep-ag . [(20130725 1831) ((wgrep (2 1 5))) "Writable ag buffer and apply the changes to files [github]" single "melpa"])
+ )
