f6d6784f6d1da1a318c306ef3276ff4d4831919a
Artur Malabarba <bruce.connor.am@gmail.com>
2013-08-15 17:55:10 +0100

    Thu Aug 15 17:55:10 BST 2013

--- gnu/gnu-2013-08-15-updated-since-4.el
+++ gnu/gnu-2013-08-15-updated-since-4.el
@@ -5,5 +5,6 @@
   (vlf . [(0 9 1) nil "View Large Files" single "gnu"])
   (rainbow-mode . [(0 9) nil "Colorize color names in buffers" single "gnu"])
   (org . [(20130812) nil "Outline-based notes management and organizer" tar "gnu"])
+  (js2-mode . [(20130619) ((emacs (24 1))) "Improved JavaScript editing mode" tar "gnu"])
   (adaptive-wrap . [(0 5) nil "Smart line-wrapping with wrap-prefix" single "gnu"])
  )
