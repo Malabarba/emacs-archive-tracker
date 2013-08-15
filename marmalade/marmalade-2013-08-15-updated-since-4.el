8c7e84f529aa3ebc6e923b49fbf0620ba6e19d73
Artur Malabarba <bruce.connor.am@gmail.com>
2013-08-15 15:34:51 +0100

    Thu Aug 15 15:34:51 BST 2013

--- marmalade/marmalade-2013-08-15-updated-since-4.el
+++ marmalade/marmalade-2013-08-15-updated-since-4.el
@@ -33,7 +33,7 @@
   (elpy . [(1 1) ((auto-complete (1 4)) (fuzzy (0 1)) (yasnippet (0 8)) (virtualenv (1 2)) (highlight-indentation (0 5 0)) (find-file-in-project (3 2)) (idomenu (0 1)) (nose (0 1 1)) (iedit (0 97))) "Emacs Lisp Python Environment" tar "marmalade"])
   (auto-indent-mode . [(0 103) nil "Auto indent Minor mode" single "marmalade"])
   (lispyscript-mode . [(0 3 0) nil "Major mode for LispyScript code." single "marmalade"])
-  (dash . [(1 6 0) nil "A modern list library for Emacs" single "marmalade"])
+  (dash . [(1 7 0) nil "A modern list library for Emacs" single "marmalade"])
   (git-commit-mode . [(0 13) nil "Major mode for editing git commit messages -*- lexical-binding: t; -*-" single "marmalade"])
   (exec-path-from-shell . [(1 6) nil "Get environment variables such as $PATH from the shell" single "marmalade"])
   (fill-column-indicator . [(1 85) nil "Graphically indicate the fill column" single "marmalade"])
