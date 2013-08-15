5b3fe5a89588241f1efe662edaf75d1d490eb0a6
Artur Malabarba <bruce.connor.am@gmail.com>
2013-08-15 05:33:57 +0100

    Thu Aug 15 05:33:57 BST 2013

--- marmalade/marmalade-2013-08-15-updated-since-4.el
+++ marmalade/marmalade-2013-08-15-updated-since-4.el
@@ -2,7 +2,7 @@
   (aok . [(0 1) nil "various useful ways to do `multi-occur'" single "marmalade"])
   (magit-tramp . [(0 1 0) ((magit (1 2 0))) "git method for TRAMP" single "marmalade"])
   (tronesque-theme . [(1 2) nil "Color Theme based on Tron universe." single "marmalade"])
-  (helm-recoll . [(1 1) ((helm (20130806 1505))) "helm interface for the recoll desktop search tool." single "marmalade"])
+  (helm-recoll . [(20130815 0) ((helm (20130806 1505))) "helm interface for the recoll desktop search tool." single "marmalade"])
   (god-mode . [(2 7 0) nil "God-like command entering minor mode" single "marmalade"])
   (heroku-theme . [(1 1 0) nil "Heroku color theme" single "marmalade"])
   (wisp-mode . [(0 1 1) nil "Major mode for editing wisp: Whitespace-to-Lisp" single "marmalade"])
