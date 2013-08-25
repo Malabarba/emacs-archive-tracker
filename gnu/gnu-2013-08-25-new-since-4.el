6e2aac2f3e1f4c064acc31ed24aa3b691cd82b46
Artur Malabarba <bruce.connor.am@gmail.com>
2013-08-25 10:35:30 +0100

    Sun Aug 25 10:35:30 BST 2013

--- gnu/gnu-2013-08-25-new-since-4.el
+++ gnu/gnu-2013-08-25-new-since-4.el
@@ -1,5 +1,6 @@
 '(
   (websocket . [(1 1) nil "Emacs WebSocket client and server" tar "gnu"])
+  (w3 . [(4 0 48) nil "Fully customizable, largely undocumented web browser for Emacs" tar "gnu"])
   (temp-buffer-browse . [(1 0) nil "temp buffer browse mode" single "gnu"])
   (dismal . [(1 5) ((cl-lib (0))) "Dis Mode Ain't Lotus: Spreadsheet program Emacs" tar "gnu"])
  )
