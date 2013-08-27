ebf59fc1c85cbc234b385a28f451390dfe8891ff
Artur Malabarba <bruce.connor.am@gmail.com>
2013-08-27 16:41:01 +0100

    Tue Aug 27 16:41:01 BST 2013

--- ALL/ALL-2013-08-27-new-since-4.el
+++ ALL/ALL-2013-08-27-new-since-4.el
@@ -25,7 +25,7 @@
   (helm-recoll . [(20130815 440) ((helm (20130806 1505))) "helm interface for the recoll desktop search tool. [github]" single "melpa"])
   (heroku-theme . [(20130810 43) nil "Heroku color theme [github]" single "melpa"])
   (ido-at-point . [(20130818 22) ((emacs (24)) (cl-lib (0 3))) "ido-style completion-at-point [github]" single "melpa"])
-  (insert-shebang . [(20130825 106) nil "Inserts shebang line automatically [github]" tar "melpa"])
+  (insert-shebang . [(20130827 1513) nil "Inserts shebang line automatically [github]" tar "melpa"])
   (kaesar . [(20130827 1323) nil "Another AES algorithm encrypt/decrypt string with password. [github]" single "melpa"])
   (kaesar-file . [(20130826 1429) ((kaesar (0 1 1))) "Encrypt/Decrypt file by AES with password. [github]" single "melpa"])
   (kaesar-mode . [(20130826 1429) ((kaesar (0 1 1))) "Encrypt/Decrypt buffer by AES with password. [github]" single "melpa"])
