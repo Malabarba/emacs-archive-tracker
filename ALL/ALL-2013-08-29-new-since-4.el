d107b409ccbbb989a6f27ecdc8bff72065111c31
Artur Malabarba <bruce.connor.am@gmail.com>
2013-08-29 20:45:56 +0100

    Thu Aug 29 20:45:56 BST 2013

--- ALL/ALL-2013-08-29-new-since-4.el
+++ ALL/ALL-2013-08-29-new-since-4.el
@@ -66,6 +66,7 @@
   (virtualenvwrapper . [(20130805 2238) ((dash (1 5 0)) (s (1 6 1))) "a modern virtualenv tool for Emacs [github]" single "melpa"])
   (wedge-ws . [(20130804 2311) nil "Wedge whitespace between columns in text [github]" single "melpa"])
   (wgrep-ag . [(20130725 1831) ((wgrep (2 1 5))) "Writable ag buffer and apply the changes to files [github]" single "melpa"])
+  (winpoint-ignore-dired . [(1 1) ((winpoint (1 0))) "Have winpoint ignore dired-mode buffers" single "marmalade"])
   (tox . [(0 1) nil "Launch current python test with tox." single "marmalade"])
   (smart-whitespace-comment-fixup . [(1 0) nil "Enables advice around yanking/killing lines that auto-indents and formats properly" single "marmalade"])
   (smtpmail-multi . [(0 3) nil "Use different smtp servers for sending mail" single "marmalade"])
--- marmalade/marmalade-2013-08-29-updated-since-4.el
+++ marmalade/marmalade-2013-08-29-updated-since-4.el
@@ -1,4 +1,5 @@
 '(
+  (winpoint-ignore-dired . [(1 1) ((winpoint (1 0))) "Have winpoint ignore dired-mode buffers" single "marmalade"])
   (tox . [(0 1) nil "Launch current python test with tox." single "marmalade"])
   (insert-shebang . [(0 9 1) nil "Inserts shebang line automatically" tar "marmalade"])
   (smart-whitespace-comment-fixup . [(1 0) nil "Enables advice around yanking/killing lines that auto-indents and formats properly" single "marmalade"])
