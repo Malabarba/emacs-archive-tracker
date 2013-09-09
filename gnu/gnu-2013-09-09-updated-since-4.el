19806cdc4972da56df7919c5ae00a4c0342a9254
Artur Malabarba <bruce.connor.am@gmail.com>
2013-09-09 11:09:55 +0100

    Mon Sep  9 11:09:55 BST 2013

--- gnu/gnu-2013-09-09-updated-since-4.el
+++ gnu/gnu-2013-09-09-updated-since-4.el
@@ -4,7 +4,7 @@
   (vlf . [(0 9 1) nil "View Large Files" single "gnu"])
   (shen-mode . [(0 1) nil "A major mode for editing shen source code" tar "gnu"])
   (rainbow-mode . [(0 9) nil "Colorize color names in buffers" single "gnu"])
-  (org . [(20130906) nil "Outline-based notes management and organizer" tar "gnu"])
+  (org . [(20130909) nil "Outline-based notes management and organizer" tar "gnu"])
   (notes-mode . [(1 30) nil "Indexing system for on-line note-taking" tar "gnu"])
   (muse . [(3 20) nil "Authoring and publishing tool for Emacs" tar "gnu"])
   (js2-mode . [(20130619) ((emacs (24 1))) "Improved JavaScript editing mode" tar "gnu"])
