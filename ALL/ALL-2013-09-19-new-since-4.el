dab49b0d516640dd8ed82b8fc5649c9168526755
Artur Malabarba <bruce.connor.am@gmail.com>
2013-09-19 13:38:13 +0100

    Thu Sep 19 13:38:13 BST 2013

--- ALL/ALL-2013-09-19-new-since-4.el
+++ ALL/ALL-2013-09-19-new-since-4.el
@@ -90,7 +90,7 @@
   (sensitive . [(20130912 2135) nil "No description available. [github]" single "melpa"])
   (sequences . [(20130908 1422) ((emacs (24))) "Ports of some Clojure sequence functions. [github]" single "melpa"])
   (sequential-command . [(20100504 855) nil "Many commands into one command [wiki]" tar "melpa"])
-  (soft-charcoal-theme . [(20130918 1649) nil "Dark charcoal theme with soft colors [github]" single "melpa"])
+  (soft-charcoal-theme . [(20130919 1008) nil "Dark charcoal theme with soft colors [github]" single "melpa"])
   (soft-morning-theme . [(20130917 1850) nil "Emacs24 theme with a light background. [github]" single "melpa"])
   (sparkline . [(20130803 1442) nil "Make sparkline images from a list of numbers [github]" single "melpa"])
   (speechd-el . [(20130828 2109) nil "Client to speech synthesizers and Braille displays. [git]" tar "melpa"])
