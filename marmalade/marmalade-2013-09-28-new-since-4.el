8846917ffdcd1b961c5a72182224cf833d875ec2
Artur Malabarba <bruce.connor.am@gmail.com>
2013-09-28 11:40:11 +0100

    Sat Sep 28 11:40:11 BST 2013

--- marmalade/marmalade-2013-09-28-new-since-4.el
+++ marmalade/marmalade-2013-09-28-new-since-4.el
@@ -1,7 +1,7 @@
 '(
   (java-file-create . [(1 0) nil "automatically insert contents of empty java files" single "marmalade"])
   (isgd . [(20130927) nil "Shorten URLs using the isgd.com shortener service" single "marmalade"])
-  (whitespace-cleanup-mode . [(0 3) nil "Intelligently call whitespace-cleanup on save" single "marmalade"])
+  (whitespace-cleanup-mode . [(0 4) nil "Intelligently call whitespace-cleanup on save" single "marmalade"])
   (highlight-current-line . [(0 57) nil "highlight line where the cursor is." single "marmalade"])
   (hl-spotlight . [(0) nil "Extension of hl-line.el to spotlight current few lines." single "marmalade"])
   (f . [(0 11 0) ((s (1 7 0)) (dash (2 2 0))) "Modern API for working with files and directories" single "marmalade"])
