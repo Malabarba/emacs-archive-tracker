ad09610697030c9214cc3a0a621505d3ba06b5cd
Artur Malabarba <bruce.connor.am@gmail.com>
2013-10-05 12:35:25 +0100

    Sat Oct  5 12:35:25 BST 2013

--- marmalade/marmalade-2013-10-05-updated-since-4.el
+++ marmalade/marmalade-2013-10-05-updated-since-4.el
@@ -1,5 +1,5 @@
 '(
-  (ac-slime . [(0 4) nil "An auto-complete source using slime completions" single "marmalade"])
+  (ac-slime . [(0 5) nil "An auto-complete source using slime completions" single "marmalade"])
   (scheme-here . [(0 3) nil "cmuscheme extension for multiple inferior processes" single "marmalade"])
   (awk-it . [(0 77) nil "Run AWK interactively on region!" single "marmalade"])
   (page-break-lines . [(0 8) nil "Display ugly ^L page breaks as tidy horizontal lines" single "marmalade"])
