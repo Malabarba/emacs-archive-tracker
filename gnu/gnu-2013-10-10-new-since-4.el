d9c2128f1450bc0f2e0a8aa64976ab9da60bf624
Artur Malabarba <bruce.connor.am@gmail.com>
2013-10-10 01:05:32 +0100

    Thu Oct 10 01:05:32 BST 2013

---
+++ gnu/gnu-2013-10-10-new-since-4.el
@@ -0,0 +1,7 @@
+'(
+  (websocket . [(1 1) nil "Emacs WebSocket client and server" tar ((:url . "http://elpa.gnu.org/packages/websocket.html")) "gnu"])
+  (w3 . [(4 0 49) nil "Fully customizable, largely undocumented web browser for Emacs" tar ((:url . "http://elpa.gnu.org/packages/w3.html")) "gnu"])
+  (temp-buffer-browse . [(1 0) nil "temp buffer browse mode" single ((:url . "http://elpa.gnu.org/packages/temp-buffer-browse.html")) "gnu"])
+  (dismal . [(1 5) ((cl-lib (0))) "Dis Mode Ain't Lotus: Spreadsheet program Emacs" tar ((:url . "http://elpa.gnu.org/packages/dismal.html")) "gnu"])
+  (adjust-parens . [(1 1) nil "Indent and dedent Lisp code, automatically adjust close parens" tar ((:url . "http://elpa.gnu.org/packages/adjust-parens.html")) "gnu"])
+ )
