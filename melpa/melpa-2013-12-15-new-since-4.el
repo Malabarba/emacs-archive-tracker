5c9fc574e3a4b41e2abae53c1065e3169d255104
Artur Malabarba <bruce.connor.am@gmail.com>
2013-12-15 21:33:36 +0000

    Sun Dec 15 21:33:36 GMT 2013

--- melpa/melpa-2013-12-15-new-since-4.el
+++ melpa/melpa-2013-12-15-new-since-4.el
@@ -146,7 +146,7 @@
   (helm-rb . [(20131123 839) ((helm (1 0)) (helm-ag-r (20131123))) "Search Ruby's method by ag and display helm" tar "melpa"])
   (helm-recoll . [(20130905 1822) ((helm (1 5 4))) "helm interface for the recoll desktop search tool." single "melpa"])
   (helm-spotify . [(20131014 1421) ((helm (0 0 0)) (multi (2 0 0))) "Control Spotify with Helm." single "melpa"])
-  (helm-swoop . [(20131214 1847) ((helm (1 0)) (emacs (24))) "Efficiently hopping squeezed lines powered by helm interface" tar "melpa"])
+  (helm-swoop . [(20131215 1002) ((helm (1 0)) (emacs (24))) "Efficiently hopping squeezed lines powered by helm interface" tar "melpa"])
   (help+ . [(20130824 1207) nil "Extensions to `help.el'." single "melpa"])
   (help-fns+ . [(20130824 1127) nil "Extensions to `help-fns.el'." single "melpa"])
   (help-mode+ . [(20130824 1159) nil "Extensions to `help-mode.el'" single "melpa"])
