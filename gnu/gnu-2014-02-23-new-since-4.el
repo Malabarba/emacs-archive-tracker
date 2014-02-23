fece58bca556d02e2b44d7ea62789da0fbcedbe9
Artur Malabarba <bruce.connor.am@gmail.com>
2014-02-23 12:21:02 +0000

    Sun Feb 23 12:21:02 GMT 2014

--- gnu/gnu-2014-02-23-new-since-4.el
+++ gnu/gnu-2014-02-23-new-since-4.el
@@ -3,7 +3,7 @@
   (wisi . [(1 0) ((cl-lib (0))) "Utilities for implementing an indentation/navigation engine using a generalized LALR parser" tar ((:keywords quote nil) (:url . "http://stephe-leake.org/emacs/ada-mode/emacs-ada-mode.html")) "gnu"])
   (websocket . [(1 1) nil "Emacs WebSocket client and server" tar ((:keywords quote ("communication" "websocket" "server")) (:url . "http://elpa.gnu.org/packages/websocket.html")) "gnu"])
   (w3 . [(4 0 49) nil "Fully customizable, largely undocumented web browser for Emacs" tar ((:keywords quote ("faces" "help" "comm" "news" "mail" "processes" "mouse" "hypermedia")) (:url . "http://elpa.gnu.org/packages/w3.html")) "gnu"])
-  (temp-buffer-browse . [(1 1) nil "temp buffer browse mode" single ((:url . "http://elpa.gnu.org/packages/temp-buffer-browse.html") (:keywords quote ("convenience"))) "gnu"])
+  (temp-buffer-browse . [(1 2) nil "temp buffer browse mode" single ((:url . "http://elpa.gnu.org/packages/temp-buffer-browse.html") (:keywords quote ("convenience"))) "gnu"])
   (flylisp . [(0 2) nil "Color unbalanced parentheses and parentheses inconsistent with indentation" single ((:url . "http://elpa.gnu.org/packages/flylisp.html") (:keywords quote nil)) "gnu"])
   (dismal . [(1 5) ((cl-lib (0))) "Dis Mode Ain't Lotus: Spreadsheet program Emacs" tar ((:keywords quote nil) (:url . "http://elpa.gnu.org/packages/dismal.html")) "gnu"])
   (diff-hl . [(1 5 3) ((cl-lib (0 2))) "Highlight uncommitted changes" tar ((:keywords quote ("vc" "diff")) (:url . "https://github.com/dgutov/diff-hl")) "gnu"])
