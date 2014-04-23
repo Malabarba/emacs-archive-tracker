558bba0e558046493bf50ae489e58cad2e88fa87
Artur Malabarba <bruce.connor.am@gmail.com>
2014-04-23 04:39:57 +0100

    Wed Apr 23 04:39:57 BST 2014

--- ALL/ALL-2014-04-23-updated-since-4.el
+++ ALL/ALL-2014-04-23-updated-since-4.el
@@ -634,7 +634,7 @@
   (memoize . [(20130421 1234) nil "Memoization functions" single ((:url . "https://github.com/skeeto/emacs-memoize")) "melpa"])
   (mentor . [(20130519 1540) ((xml-rpc (1 6 9))) "Frontend for the rTorrent bittorrent client" tar ((:keywords "bittorrent" "rtorrent")) "melpa"])
   (metaweblog . [(20130707 431) ((xml-rpc (1 6 8))) "An emacs library to access metaweblog based weblogs" tar nil "melpa"])
-  (mew . [(20140416 14) nil "Messaging in the Emacs World" tar nil "melpa"])
+  (mew . [(20140422 1936) nil "Messaging in the Emacs World" tar nil "melpa"])
   (mic-paren . [(20120716 1647) nil "advanced highlighting of matching parentheses" single ((:keywords "languages" "faces" "parenthesis" "matching")) "melpa"])
   (midje-mode . [(20131019 1357) ((cider (0 1 4)) (clojure-mode (1 0))) "Minor mode for running Midje tests in emacs" tar nil "melpa"])
   (migemo . [(20140305 156) nil "Japanese incremental search through dynamic pattern expansion" single ((:url . "https://github.com/emacs-jp/migemo")) "melpa"])
