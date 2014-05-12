138f799aa74d5917ef1cbc9788cd7729ca84466d
Artur Malabarba <bruce.connor.am@gmail.com>
2014-05-12 11:49:01 +0100

    Mon May 12 11:49:01 BST 2014

--- melpa/melpa-2014-05-12-updated-since-4.el
+++ melpa/melpa-2014-05-12-updated-since-4.el
@@ -302,7 +302,7 @@
   (flex-isearch . [(20130508 1503) nil "Flex matching (like ido) in isearch." single ((:url . "https://bitbucket.org/jpkotta/flex-isearch") (:keywords "convenience" "search")) "melpa"])
   (flx . [(20140420 1131) ((cl-lib (0 3))) "fuzzy matching with good sorting" single ((:url . "https://github.com/lewang/flx")) "melpa"])
   (flx-ido . [(20140416 1727) ((flx (0 1)) (cl-lib (0 3))) "flx integration for ido" single ((:url . "https://github.com/lewang/flx")) "melpa"])
-  (flycheck . [(20140422 657) ((s (1 9 0)) (dash (2 4 0)) (f (0 11 0)) (pkg-info (0 4)) (cl-lib (0 3)) (emacs (24 1))) "On-the-fly syntax checking (Flymake done right)" tar ((:url . "https://flycheck.readthedocs.org") (:keywords "convenience" "languages" "tools")) "melpa"])
+  (flycheck . [(20140512 249) ((s (1 9 0)) (dash (2 4 0)) (f (0 11 0)) (pkg-info (0 4)) (cl-lib (0 3)) (emacs (24 1))) "On-the-fly syntax checking (Flymake done right)" tar ((:url . "https://flycheck.readthedocs.org") (:keywords "convenience" "languages" "tools")) "melpa"])
   (flycheck-color-mode-line . [(20131125 2138) ((flycheck (0 15)) (dash (1 2)) (emacs (24 1))) "Change mode line color with Flycheck status" single ((:keywords "convenience" "language" "tools")) "melpa"])
   (flymake-coffee . [(20130828 258) ((flymake-easy (0 1))) "A flymake handler for coffee script" single ((:url . "https://github.com/purcell/flymake-coffee")) "melpa"])
   (flymake-css . [(20121104 1104) ((flymake-easy (0 1))) "Flymake support for css using csslint" single ((:url . "https://github.com/purcell/flymake-css")) "melpa"])
