e79375ff6889c6b88a0ca8d54e5008c6811b47be
Artur Malabarba <bruce.connor.am@gmail.com>
2014-05-29 21:54:24 +0100

    Thu May 29 21:54:24 BST 2014

--- ALL/ALL-2014-05-29-new-since-4.el
+++ ALL/ALL-2014-05-29-new-since-4.el
@@ -17,7 +17,7 @@
   (addressbook-bookmark . [(20140430 10) ((emacs (24))) "An address book based on Standard Emacs bookmarks." single ((:url . "https://github.com/thierryvolpiatto/addressbook-bookmark")) "melpa"])
   (afternoon-theme . [(20140104 1059) ((emacs (24 1))) "Dark color theme with a deep blue background" single ((:url . "http://github.com/osener/emacs-afternoon-theme") (:keywords "themes")) "melpa"])
   (alect-themes . [(20140522 206) nil "Configurable color themes (light, dark and black) for Emacs 24" tar nil "melpa"])
-  (amd-mode . [(20140514 138) ((projectile (0 10 0)) (s (1 9 0)) (f (0 16 2)) (dash (2 5 0)) (makey (0 3)) (js2-mode (20140114)) (js2-refactor (0 6 1))) "Minor mode for handling JavaScript AMD module requirements." single ((:keywords "javascript" "amd" "projectile")) "melpa"])
+  (amd-mode . [(20140529 1216) ((projectile (0 10 0)) (s (1 9 0)) (f (0 16 2)) (dash (2 5 0)) (makey (0 3)) (js2-mode (20140114)) (js2-refactor (0 6 1))) "Minor mode for handling JavaScript AMD module requirements." single ((:keywords "javascript" "amd" "projectile")) "melpa"])
   (ample-zen-theme . [(20131129 440) nil "AmpleZen Theme for Emacs 24" single ((:url . "https://github.com/mjwall/ample-zen") (:keywords "theme" "dark" "emacs 24")) "melpa"])
   (anaconda-mode . [(20140506 308) ((emacs (24))) "Code navigation, documentation lookup and completion for Python" tar ((:url . "https://github.com/proofit404/anaconda-mode")) "melpa"])
   (ant . [(20131227 2307) nil "helpers for compiling with ant" single ((:keywords "compilation" "ant" "java")) "melpa"])
@@ -77,7 +77,7 @@
   (cg . [(20140306 620) nil "major mode for editing Constraint Grammar files" single ((:url . "http://beta.visl.sdu.dk/constraint_grammar.html") (:keywords "languages")) "melpa"])
   (cherry-blossom-theme . [(20140126 1025) ((emacs (24 0))) "a soothing color theme for Emacs24." single ((:url . "https://github.com/byels/emacs-cherry-blossom-theme")) "melpa"])
   (chruby . [(20140223 1028) nil "Emacs integration for chruby" single ((:url . "http://www.emacswiki.org/emacs/ChrubyEl") (:keywords "ruby" "chruby")) "melpa"])
-  (cider . [(20140529 536) ((clojure-mode (2 0 0)) (cl-lib (0 3)) (dash (2 4 1)) (pkg-info (0 4)) (emacs (24))) "Clojure Integrated Development Environment and REPL" tar ((:url . "http://www.github.com/clojure-emacs/cider") (:keywords "languages" "clojure" "cider")) "melpa"])
+  (cider . [(20140529 919) ((clojure-mode (2 0 0)) (cl-lib (0 3)) (dash (2 4 1)) (pkg-info (0 4)) (emacs (24))) "Clojure Integrated Development Environment and REPL" tar ((:url . "http://www.github.com/clojure-emacs/cider") (:keywords "languages" "clojure" "cider")) "melpa"])
   (cider-decompile . [(20131116 1002) ((cider (0 3 0)) (javap-mode (9))) "decompilation extension for cider" single ((:url . "http://www.github.com/clojure-emacs/cider-decompile") (:keywords "languages" "clojure" "cider")) "melpa"])
   (cider-tracing . [(20131018 738) ((cider (0 3 0)) (clojure-mode (2 1 0))) "Tracing extension for CIDER" single ((:url . "http://www.github.com/clojure-emacs/cider-tracing") (:keywords "languages" "clojure" "cider")) "melpa"])
   (cl-lib-highlight . [(20140127 1312) ((cl-lib (0 3))) "full cl-lib font-lock highlighting" single ((:url . "https://github.com/skeeto/cl-lib-highlight")) "melpa"])
@@ -530,7 +530,7 @@
   (projekt . [(20140424 333) ((emacs (24))) "some kind of staging for CVS" single nil "melpa"])
   (prosjekt . [(20140512 525) nil "a software project tool for emacs" tar ((:url . "https://github.com/abingham/prosjekt")) "melpa"])
   (psession . [(20140412 2157) ((emacs (24)) (cl-lib (0 5))) "Persistent save of elisp objects." single ((:url . "https://github.com/thierryvolpiatto/psession")) "melpa"])
-  (pt . [(20140429 1908) nil "A front-end for pt, The Platinum Searcher." single ((:url . "https://github.com/bling/pt.el") (:keywords "pt" "ack" "ag" "search")) "melpa"])
+  (pt . [(20140529 713) nil "A front-end for pt, The Platinum Searcher." single ((:url . "https://github.com/bling/pt.el") (:keywords "pt" "ack" "ag" "grep" "search")) "melpa"])
   (pungi . [(20140313 343) ((jedi (20140310 2333))) "Integrates jedi with virtualenv and buildout python environments" single ((:keywords "convenience")) "melpa"])
   (puppetfile-mode . [(20131017 833) nil "Syntax highlighting for Puppetfiles" single nil "melpa"])
   (pushbullet . [(20140528 1108) ((grapnel (0 5 2)) (json (1 2)) (ht (2 0))) "Emacs client for the PushBullet Android app" single ((:url . "http://www.github.com/theanalyst/pushbullet.el") (:keywords "convenience")) "melpa"])
@@ -698,171 +698,32 @@
   (zotxt . [(20140420 1034) nil "Tools to integrate emacs with Zotero via the zotxt plugin." tar nil "melpa"])
   (ztree . [(20140409 726) nil "Several text-tree applications" tar nil "melpa"])
   (ac-R . [(0 2) nil "Autocompletion routines for R" single "marmalade"])
-  (ac-python . [(20110519) "No commentary." "Simple Python Completion Source for Auto-Complete" single "marmalade"])
+  (ac-python . [(20110519) ((auto-complete (1 4))) "Simple Python Completion Source for Auto-Complete" single "marmalade"])
   (auto-complete-etags . [(0 2) ((auto-complete (1 3))) "Auto-complete etags" single "marmalade"])
   (auto-complete-octave . [(0 1) nil "Auto-complete for Octave" single "marmalade"])
   (auto-complete-verilog . [(0 0) nil "Verukig" single "marmalade"])
-  (clojure-project . [(1 0) "No commentary." "Extends project-mode for Clojure projects" single "marmalade"])
-  (cmuclojure . [(0 2) "; Run clojure in your buffer, and send your definition to your buffer immediately.
-;; Install: (require 'cmuclojure)
-;; Keybinding: C-c M-j run-clojure
-;;             C-c e clojure-send-definition
-;;             C-c C-e clojure-send-definition-and-go
-;;             C-c r clojure-send-region
-;;
-;;; License
-;;
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-" "Clojure process in a buffer" single "marmalade"])
+  (clojure-project . [(1 0) ((project-mode (1 0))) "Extends project-mode for Clojure projects" single "marmalade"])
   (color-theme-tangotango . [(0 0 2) ((color-theme (6 6 1))) "Tango Palette color theme for Emacs." single "marmalade"])
-  (comint-better-defaults . [(1 0) nil "better defaults for comint-derived modes" single "marmalade"])
-  (cubicle-mode . [(0 1) "No commentary." "Cubicle major mode for emacs" single "marmalade"])
-  (discord . [(0 5) "No commentary." "Discordian dates for calendar" single "marmalade"])
-  (dummy-package . [(0 0 3) ((timeclock (2 6 1))) "a fake package for the marmalade test suite" single "marmalade"])
   (eh-common . [(0 0 1) nil "Tumashu's  emacs functions" single "marmalade"])
   (erc-nick-notify . [(0 2 1) nil "Notify popup for ERC" single "marmalade"])
-  (eshell-fringe-status . [(1 0 0) nil "Show last status in fringe" single "marmalade"])
-  (exercism . [(0 0 1) nil "An Emacs mode for submitting current file to exercism.io" single "marmalade"])
-  (flycheck-tcl . [(0 4) ((flycheck (0 17))) "A flycheck checker for Tcl using ActiveState's tclchecker" single "marmalade"])
-  (foo . [(3 0 20) "No commentary." "Sass major mode" single "marmalade"])
-  (gecf . [(0 1 0) nil "G\366ktu's Emacs configuration framework." single "marmalade"])
-  (gratuitous-dark-theme . [(1 3) "No commentary." "A theme originally by 'cofi' from #emacs, Freenode. It is a combination of Monokai and several other themes. The modeline has been modelled after AwesomeWM because it looks cool." single "marmalade"])
+  (foo . [(3 0 20) ((haml-mode (3 0 20))) "Sass major mode" single "marmalade"])
   (guile-scheme . [(0 1) nil "Guile Scheme editing mode" single "marmalade"])
-  (helm-delicious . [(1 3) nil "helm extensions for delicious bookmarks" single "marmalade"])
-  (initchart . [(0 1 1) ((cl-lib (0 3))) "Emacs' init process performance visualization" single "marmalade"])
-  (jam-mode . [(0 3) nil "Font-lock support for Jam files" single "marmalade"])
   (jasmin . [(1 2) nil "major editing mode for Jasmin Java bytecode assembler files" single "marmalade"])
-  (java-file-create . [(1 0) nil "automatically insert contents of empty java files" single "marmalade"])
-  (jcuken-fix . [(1 0) "No commentary." "Map Modifier-CyrillicLetter to the underlying Modifier-LatinLetter." single "marmalade"])
-  (journal . [(1 2 1) "No commentary." "a simple org-mode based journaling mode" single "marmalade"])
-  (jujube-theme . [(0 1) "No commentary." "Pastel theme loosely based on jellybeans" single "marmalade"])
+  (jcuken-fix . [(1 0) nil "Map Modifier-CyrillicLetter to the underlying Modifier-LatinLetter." single "marmalade"])
+  (journal . [(1 2 1) nil "a simple org-mode based journaling mode" single "marmalade"])
   (lang-refactor . [(0 1 1) nil "Simple refactorings, primarily for Perl" single "marmalade"])
   (layout-restore . [(0 4) nil "keep window configuration as layout and restore it simply." single "marmalade"])
   (linear-undo . [(5 2) nil "Intuitive undo/redo." single "marmalade"])
-  (lyskom . [(20131008) nil "LysKOM elisp client." single "marmalade"])
   (master-mode . [(0 1) nil "Become an Emacs master" single "marmalade"])
   (mc-jump . [(1 0 0) nil "like \"jump-char\", but \"multiple-cursors\" friendly" single "marmalade"])
-  (navigate . [(0 1 5) nil "Seamlessly navigate between Emacs and tmux" single "marmalade"])
-  (nrepl-discover . [(0 0 1) nil "Client to load commands from nrepl server" single "marmalade"])
-  (palimpsest-mode . [(0 8) nil "Various deletion strategies when editing" single "marmalade"])
-  (parenface2 . [(1 1) "No commentary." "Provide a face for parens in lisp modes." single "marmalade"])
-  (pbcopy . [(0 1 0) nil "OS X clipboard integration for Emacs" single "marmalade"])
-  (php-completion . [(0 3) ";; complete php functions, symbols, ini directives, keywords with Anything.el's Interface.
-
-;;; Commands:
-;;
-;; Below are complete command list:
-;;
-;;
-;;; Customizable Options:
-;;
-;; Below are customizable option list:
-;;
-;;  `phpcmp-browse-function'
-;;    called with one argment URL
-;;    default is (quote browse-url)
-;;  `phpcmp-manual-url-format'
-;;    `%s' is replaced to query.
-;;    default = (cond ((ignore-errors ...) \"http://jp2.php.net/manual-lookup.php?lang=ja&pattern=%s\") (t \"http://jp2.php.net/manual-lookup.php?lang=en&pattern=%s\"))
-;;  `phpcmp-lighter'
-;;    value is displayed in the modeline when the php-completion-mode is on.
-;;    default = \" Completion\"
-;;  `phpcmp-global-enable-auto-update-tag-files'
-;;    If value is non-nil, automatically update GNU global's tag files.
-;;    default = nil
-;;  `phpcmp-showtip-timeout'
-;;    Seconds to wait before displaying a tooltip the first time.
-;;    default = 10
-;;  `phpcmp-showtip-top-adjust'
-;;    Basic adjust.
-;;    default = 40
-
-;; Installation:
-;;
-;; put `php-completion.el' somewhere in your emacs load path.
-;; add these lines to your .emacs file:
-;; -------------- .emacs -----------------------------
-;; (add-hook 'php-mode-hook
-;;           (lambda ()
-;;             (require 'php-completion)
-;;             (php-completion-mode t)
-;;             (define-key php-mode-map (kbd \"C-o\") 'phpcmp-complete)))
-;; ---------------------------------------------------
-
-;; Cooperation with auto-complete.el:
-;;
-;; add these lines to your .emacs file:
-;; (add-hook  'php-mode-hook
-;;            (lambda ()
-;;              (when (require 'auto-complete nil t)
-;;                (make-variable-buffer-local 'ac-sources)
-;;                (add-to-list 'ac-sources 'ac-source-php-completion)
-;;                ;; if you like patial match,
-;;                ;; use `ac-source-php-completion-patial' instead of `ac-source-php-completion'.
-;;                ;; (add-to-list 'ac-sources 'ac-source-php-completion-patial)
-;;                (auto-complete-mode t))))
-
-;; Features that might be required by this library:
-;;
-;;   `anything', `anything-match-plugin', `auto-complete',
-;;   `backquote', `browse-url', `button', `bytecomp', `cl', `etags',
-;;   `help-fns', `mail-prsvr', `mm-util', `popup', `ring', `rx',
-;;   `thingatpt', `timer', `url-parse', `url-util', `url-vars',
-;;   `warnings'.
-;;
-
-(require 'cl)
-(require 'rx)
-(require 'browse-url)
-(require 'url-util)
-(require 'etags)
-(require 'thingatpt)
-
-(require 'anything)
-(require 'anything-match-plugin)
-(require 'auto-complete)
-
-(defvar phpcmp-version 0.03)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Customize Variables ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-" "complete everything PHP with Anything.el" single "marmalade"])
-  (pickup . [(0 0 3) nil "pickup file." single "marmalade"])
-  (pkg-info-dummy-package . [(3 4 2 1) nil "pkg-info: Dummy package for unit tests" single "marmalade"])
-  (proxy-mode . [(0 9) nil "Provides proxy minor mode." single "marmalade"])
-  (pycomplete . [(1 0) "No commentary." "Complete symbols at point using Pymacs." single "marmalade"])
+  (php-completion . [(0 3) nil "complete everything PHP with Anything.el" single "marmalade"])
+  (pycomplete . [(1 0) nil "Complete symbols at point using Pymacs." single "marmalade"])
   (setup-cygwin . [(21 0) nil "Set up Emacs for using Cygwin" single "marmalade"])
-  (slime-scratch . [(1 0 0) "No commentary." "Imitate Emacs' *scratch* buffer" single "marmalade"])
-  (smart-whitespace-comment-fixup . [(1 0) nil "Enables advice around yanking/killing lines that auto-indents and formats properly" single "marmalade"])
-  (smtpmail-multi . [(0 6) nil "Use different smtp servers for sending mail" single "marmalade"])
-  (steady-theme . [(0 1 0) nil "A steady theme for Emacs" single "marmalade"])
-  (tango-2 . [(1 0 0) "No commentary." "Tango 2 color theme for GNU Emacs 24" single "marmalade"])
-  (tbemail . [(0 2) nil "Provide syntax highlighting for email editing via" single "marmalade"])
-  (timestamper . [(0 1 0) "No commentary." "A minor mode for easy timelogging" single "marmalade"])
-  (troncle . [(0 1 2) ((cider (0 5 0))) "Emacs convenience functions for tracing clojure code" single "marmalade"])
-  (tsql . [(1 0) nil "customizes sql.el for T-SQL" single "marmalade"])
-  (typescript . [(0 1 1) nil "Major mode for editing typescript" single "marmalade"])
+  (slime-scratch . [(1 0 0) ((slime (20100404))) "Imitate Emacs' *scratch* buffer" single "marmalade"])
+  (tango-2 . [(1 0 0) nil "Tango 2 color theme for GNU Emacs 24" single "marmalade"])
   (vimpulse . [(0 5) nil "emulates Vim's most useful features" single "marmalade"])
   (viper-in-more-modes . [(0 1 3) nil "vi-like keybindings for various Emacs modes" single "marmalade"])
-  (window-margin . [(0 1) nil "automatic margins for visual-line-mode wrapping" single "marmalade"])
-  (winpoint-ignore-dired . [(1 1) ((winpoint (1 0))) "Have winpoint ignore dired-mode buffers" single "marmalade"])
-  (wisp . [(0 1 4) nil "Tools for wisp: the Whitespace-to-Lisp preprocessor" single "marmalade"])
-  (el-init . [(20130416) nil "Loader for configuration files" tar "marmalade"])
-  (elpa-mirror . [(1 1 2) nil "ELPA mirror from locally installed packages is easy" tar "marmalade"])
-  (nose-mode . [(0 1) ((nose (0 1 1))) "Minor mode for running nose tests." tar "marmalade"])
-  (org2nikola . [(0 0 8) nil "export html and meta data used by static blog like nikola from org file" tar "marmalade"])
-  (ps-ccrypt . [(1 10) nil "reading/writing/loading encrypted files" tar "marmalade"])
-  (zone-matrix . [(0 0 1) nil "Matrix themed Zone mode" tar "marmalade"])
+  (xahk-mode . [(1 2 1) nil "Major mode for editing AHK (Autohotkey) scripts." single "marmalade"])
   (xpm . [(1 0 1) nil "edit XPM images" tar ((:keywords "multimedia" "xpm") (:url . "http://elpa.gnu.org/packages/xpm.html")) "gnu"])
   (wpuzzle . [(1 1) nil "find as many word in a given time" single ((:url . "http://elpa.gnu.org/packages/wpuzzle.html") (:keywords)) "gnu"])
   (wisi . [(1 0 4) ((cl-lib (0 4)) (emacs (24 2))) "Utilities for implementing an indentation/navigation engine using a generalized LALR parser" tar ((:url . "http://stephe-leake.org/emacs/ada-mode/emacs-ada-mode.html")) "gnu"])
