cdfbaf7c46747b520a09d92b6be3775959317668
Artur Malabarba <bruce.connor.am@gmail.com>
2014-07-10 10:07:24 +0100

    Thu Jul 10 10:07:24 BST 2014

--- ALL/ALL-2014-07-10-new-since-4.el
+++ ALL/ALL-2014-07-10-new-since-4.el
@@ -626,7 +626,7 @@
   (roguel-ike . [(20140227 729) ((popup (0 5 0))) "A coffee-break roguelike" tar nil "melpa"])
   (rotate . [(20140123 116) nil "Rotate the layout of emacs" single ((:url . "https://github.com/daic-h/emacs-rotate") (:keywords "window" "layout")) "melpa"])
   (rpm-spec-mode . [(20140120 50) nil "RPM spec file editing commands for Emacs/XEmacs" single ((:keywords "unix" "languages")) "melpa"])
-  (rtags . [(20140708 1208) nil "No description available." tar nil "melpa"])
+  (rtags . [(20140710 6) nil "No description available." tar nil "melpa"])
   (ruby-additional . [(20140422 1903) ((emacs (24 3)) (ruby-mode (1 2))) "ruby-mode extensions yet to be merged into Emacs" tar ((:url . "http://svn.ruby-lang.org/cgi-bin/viewvc.cgi/trunk/misc/") (:keywords "ruby" "languages")) "melpa"])
   (ruby-refactor . [(20140104 1354) ((ruby-mode (1 2))) "A minor mode which presents various Ruby refactoring helpers." single ((:url . "https://github.com/ajvargo/ruby-refactor") (:keywords "refactor" "ruby")) "melpa"])
   (runner . [(20140106 758) nil "Improved \"open with\" suggestions for dired" single ((:url . "https://github.com/thamer/runner") (:keywords "shell command" "dired" "file extension" "open with")) "melpa"])
