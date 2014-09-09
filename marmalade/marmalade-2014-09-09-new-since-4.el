7f2c96d927257e405938c087f9f02269f9586b4a
Artur Malabarba <bruce.connor.am@gmail.com>
2014-09-09 10:52:45 +0100

    Tue Sep  9 10:52:45 BST 2014

--- marmalade/marmalade-2014-09-09-new-since-4.el
+++ marmalade/marmalade-2014-09-09-new-since-4.el
@@ -199,4 +199,5 @@
   (zeal-at-point . [(0 0 3) nil "Search the word at point with Zeal" single "marmalade"])
   (zenburn . [(0 1) nil "A low contrast colour theme for Emacs" single "marmalade"])
   (zone-matrix . [(0 0 1) nil "Matrix themed Zone mode" tar "marmalade"])
+  (move-line . [(0 0 1) nil "utilities for moving lines in file" single "marmalade"])
  )
