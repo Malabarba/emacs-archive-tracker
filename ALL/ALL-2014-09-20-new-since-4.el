c5b310a41b86dfdb6dd19114b5814685bff5ee6e
Artur Malabarba <bruce.connor.am@gmail.com>
2014-09-20 20:37:06 +0100

    Sat Sep 20 20:37:06 BST 2014

--- ALL/ALL-2014-09-20-new-since-4.el
+++ ALL/ALL-2014-09-20-new-since-4.el
@@ -98,7 +98,7 @@
   (chef-mode . [(20111121 700) nil "minor mode for editing an opscode chef repository" single ((:keywords "chef" "knife")) "melpa"])
   (cherry-blossom-theme . [(20140126 1025) ((emacs (24 0))) "a soothing color theme for Emacs24." single ((:url . "https://github.com/byels/emacs-cherry-blossom-theme")) "melpa"])
   (chruby . [(20140811 811) nil "Emacs integration for chruby" single ((:url . "http://www.emacswiki.org/emacs/ChrubyEl") (:keywords "ruby" "chruby")) "melpa"])
-  (cider . [(20140920 307) ((clojure-mode (3 0 0)) (cl-lib (0 5)) (dash (2 4 1)) (pkg-info (0 4)) (emacs (24)) (queue (0 1 1))) "Clojure Integrated Development Environment and REPL" tar ((:url . "http://www.github.com/clojure-emacs/cider") (:keywords "languages" "clojure" "cider")) "melpa"])
+  (cider . [(20140920 826) ((clojure-mode (3 0 0)) (cl-lib (0 5)) (dash (2 4 1)) (pkg-info (0 4)) (emacs (24)) (queue (0 1 1))) "Clojure Integrated Development Environment and REPL" tar ((:url . "http://www.github.com/clojure-emacs/cider") (:keywords "languages" "clojure" "cider")) "melpa"])
   (cider-decompile . [(20131116 1002) ((cider (0 3 0)) (javap-mode (9))) "decompilation extension for cider" single ((:url . "http://www.github.com/clojure-emacs/cider-decompile") (:keywords "languages" "clojure" "cider")) "melpa"])
   (cider-spy . [(20140831 2355) ((cider (0 5 0)) (dash (2 5 0)) (cl-lib (0 5))) "Spy on CIDER to get info" single ((:url . "http://www.github.com/jonpither/cider-spy") (:keywords "languages" "clojure" "cider" "nrepl")) "melpa"])
   (cl-lib-highlight . [(20140127 1312) ((cl-lib (0 3))) "full cl-lib font-lock highlighting" single ((:url . "https://github.com/skeeto/cl-lib-highlight")) "melpa"])
