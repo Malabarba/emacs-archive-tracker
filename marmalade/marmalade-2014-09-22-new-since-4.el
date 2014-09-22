e26367f9f4df0cbb675bd517032b773d8d3740ec
Artur Malabarba <bruce.connor.am@gmail.com>
2014-09-22 09:03:25 +0100

    Mon Sep 22 09:03:25 BST 2014

--- marmalade/marmalade-2014-09-22-new-since-4.el
+++ marmalade/marmalade-2014-09-22-new-since-4.el
@@ -204,4 +204,5 @@
   (zeal-at-point . [(0 0 3) nil "Search the word at point with Zeal" single "marmalade"])
   (zenburn . [(0 1) nil "A low contrast colour theme for Emacs" single "marmalade"])
   (zone-matrix . [(0 0 1) nil "Matrix themed Zone mode" tar "marmalade"])
+  (dirtrack-buffer-name-track-mode . [(1 0 0) nil "minor mode to cause shell buffers to reflect the working directory" single "marmalade"])
  )
