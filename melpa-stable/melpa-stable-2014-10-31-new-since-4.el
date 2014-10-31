f3f9b78534bd1c7ba6c673c7cd2b0cfdee2a2e21
Artur Malabarba <bruce.connor.am@gmail.com>
2014-10-31 02:40:04 +0000

    Fri Oct 31 02:40:04 GMT 2014

---
+++ melpa-stable/melpa-stable-2014-10-31-new-since-4.el
@@ -0,0 +1,3 @@
+'(
+  (aggressive-indent . [(0 3 1) ((emacs (24 1)) (names (0 5)) (cl-lib (0 5))) "Minor mode to aggressively keep your code always indented" single ((:url . "http://github.com/Bruce-Connor/aggressive-indent-mode") (:keywords "indent" "lisp" "maint" "tools")) "melpa-stable"])
+ )
