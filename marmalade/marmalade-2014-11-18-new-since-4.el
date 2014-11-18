947af0ffaa558a6c7df2a576836906673a08537d
Artur Malabarba <bruce.connor.am@gmail.com>
2014-11-18 17:02:45 +0000

    Tue Nov 18 17:02:45 GMT 2014

--- marmalade/marmalade-2014-11-18-new-since-4.el
+++ marmalade/marmalade-2014-11-18-new-since-4.el
@@ -18,6 +18,7 @@
   (bubbleberry-theme . [(0 1 2) ((emacs (24 1))) "A theme based on LightTable for Emacs24" single "marmalade"])
   (buffer-file-utils . [(1 0) nil "utilities operating on a buffer and the associated file" single "marmalade"])
   (cedit . [(0 0 0) nil "paredit-like commands for c-like languages" single "marmalade"])
+  (checkbox . [(0 2 1) ((emacs (24)) (cl-lib (0 5))) "Quick manipulation of textual checkboxes" single "marmalade"])
   (cider . [(0 6 0) ((clojure-mode (2 0 0)) (cl-lib (0 3)) (dash (2 4 1)) (pkg-info (0 4))) "Clojure Integrated Development Environment and REPL" tar "marmalade"])
   (cl-lib-highlight . [(1 0 0) ((cl-lib (0 3))) "full cl-lib font-lock highlighting" single "marmalade"])
   (clojure-env . [(0 0 3) nil "manage clojure environments with Emacs" single "marmalade"])
@@ -231,5 +232,4 @@
   (zeal-at-point . [(0 0 3) nil "Search the word at point with Zeal" single "marmalade"])
   (zenburn . [(0 1) nil "A low contrast colour theme for Emacs" single "marmalade"])
   (zone-matrix . [(0 0 1) nil "Matrix themed Zone mode" tar "marmalade"])
-  (checkbox . [(0 2 1) ((emacs (24)) (cl-lib (0 5))) "Quick manipulation of textual checkboxes" single "marmalade"])
  )
