d86c46b82ab8a5126d582cb60402885b13d429f8
Artur Malabarba <bruce.connor.am@gmail.com>
2014-12-01 15:23:28 +0000

    Mon Dec  1 15:23:28 GMT 2014

--- marmalade/marmalade-2014-12-01-new-since-4.el
+++ marmalade/marmalade-2014-12-01-new-since-4.el
@@ -235,4 +235,5 @@
   (zeal-at-point . [(0 0 3) nil "Search the word at point with Zeal" single "marmalade"])
   (zenburn . [(0 1) nil "A low contrast colour theme for Emacs" single "marmalade"])
   (zone-matrix . [(0 0 1) nil "Matrix themed Zone mode" tar "marmalade"])
+  (shackle . [(0 1) nil "Enforce rules for popups" single "marmalade"])
  )
