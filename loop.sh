6aaa6b76943ca28fc8add6e0606ae0b3aacb8561
Artur Malabarba <bruce.connor.am@gmail.com>
2015-03-11 18:00:17 +0000

    Wed Mar 11 18:00:17 GMT 2015

--- loop.sh
+++ loop.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-$EMACS -Q --batch --script emacs-archive-tracker.el
+[[ -z "$EMACS" ]] && echo "NO EMACS!!" && exit 1;
+    
+cd ~/.tracker/ &&
+    /usr/bin/nice $EMACS -Q --batch --script emacs-archive-tracker.el
