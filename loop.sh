13b28395404bf21291454061ac901e101b71b3bb
Artur Malabarba <bruce.connor.am@gmail.com>
2015-04-02 00:00:19 +0100

    Thu Apr  2 00:00:19 BST 2015

--- loop.sh
+++ loop.sh
@@ -3,4 +3,4 @@
 [[ -z "$EMACS" ]] && echo "NO EMACS!!" && exit 1;
     
 cd ~/.tracker/ &&
-    /usr/bin/nice $EMACS -Q --batch --script emacs-archive-tracker.el
+    /usr/bin/nice $EMACS -Q --batch --eval '(setq debug-on-error t)' --eval '(setq load-prefer-newer t)' --script emacs-archive-tracker.el
